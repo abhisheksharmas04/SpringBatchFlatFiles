68aa705c7e94ac107eb3a89accc98d3dbce234d6
abhisheksharmas04 <abhisheksharmaas04@gmail.com>
2022-09-10 12:36:57 +0530

    Project Created

---
+++ src/main/java/com/spring/batch/flat/flies/SpringBathcFlatFilesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.batch.flat.flies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBathcFlatFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBathcFlatFilesApplication.class, args);
+	}
+
+}
